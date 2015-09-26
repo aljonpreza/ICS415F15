aaaecc6dad4d3c21f6b3ce60eeef7d13103ffbb3
Aljon Preza <aljonpreza@yahoo.com>
2015-09-25 15:25:13 -1000

    Finished Half of A08

--- A08/myScript.js
+++ A08/myScript.js
@@ -1,3 +1,28 @@
 /**
  * Created by aljonp on 9/23/15.
  */
+
+
+$(document).ready(function() {
+  $(".answer").hide();
+  $(".up").hide();
+
+  $(".question").click(function() {
+    $(this).next(".answer").slideToggle(0);
+    $(this).find(".up, .down").toggle();
+
+  });
+});
+
+
+function doStuff()
+{
+  var nameElement = document.getElementById("someInput");
+  var theName = nameElement.value;
+
+  console.log(theName);
+
+  $(document).ready(function() {
+    document.getElementById("result").innerHTML = theName;
+  });
+}
